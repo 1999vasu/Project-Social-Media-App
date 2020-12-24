da125c252a757530202026db4524a8838efa4890
Vasu Gupta <vasugupta.com@gmail.com>
2020-12-24 14:58:15 +0530

    posts controllers code added

--- README.md
+++ README.md
@@ -22,3 +22,30 @@ src/db/          #db connections and model controllers
 src/public/      # html/js/css files for static part of site
 src/routes/      # express middlewares(route wise)
 ```
+
+## Business Logic
+
+### Users
+1. **Create Users**
+    creating new  user with random username
+
+### Posts
+1. **create Posts**
+    this will create a new post, required fields are:
+    - username (author of the post)
+    - title (title of the post)
+    - body (content of the post)
+
+2. **Show All Posts**
+    list all posts, we should have following support
+    - filter by username
+    - filter by title
+
+3. **Edit Users**
+
+4. **Delete Posts**
+
+### Comments
+1. **show all comments (of a user)**
+2. **show all comments (of a post)**
+3. **Add a comment**
--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,1 +1,54 @@
-const {Posts} = require("../db/models")
+const {Posts,Users} = require("../db/models")
+
+async function createNewPost(userId, title, body){
+    const post = await Posts.create({
+        title,
+        body,
+        userId
+    })
+
+    return post
+
+}
+
+async function showAllPosts(query){
+
+    const posts = await Posts.findAll({
+        include: Users
+    })
+
+    return posts
+
+}
+
+module.exports ={
+    createNewPost,
+    showAllPosts
+}
+
+// Unit testing 
+// async function task(){
+    // console.log(await createNewPost(
+    //     1,
+    //     'this is sample body',
+    //     'Body of first sample post'
+    // )
+    // ),
+    // console.log(await createNewPost(
+    //     2,
+    //     'this is another sample body',
+    //     'Body of another sample post'
+    // )
+    // )
+//     const posts = await showAllPosts()
+//     for( let p of posts){
+//         console.log(p.title)
+//         console.log(p.body)
+//         console.log(p.user.username)
+//         console.log("------------------")
+//     }
+
+// }
+
+// task()
+
