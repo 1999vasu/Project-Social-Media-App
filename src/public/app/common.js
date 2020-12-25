2cae3be02cdb93d0d8790754eea3db280787ab5f
Vasu Gupta <vasugupta.com@gmail.com>
2020-12-25 11:24:48 +0530

    view posts code added

--- src/public/app/common.js
+++ src/public/app/common.js
@@ -1,5 +1,51 @@
+// const { Json } = require("sequelize/types/lib/utils")
+
 $(()=>{
-    $('#navbar').load("components/navbar.html")
-    console.log("hello")
+    $('#navbar').load("components/navbar.html",loginIfneeded)
+    $('#footer').load('components/footer.html')
+    $('#content').load('components/all-articles.html')
+    loadPosts()
+    
 })
 
+function loginIfneeded(){
+    window.currentUser = window.localStorage.user ? JSON.parse(window.localStorage.user) : null
+    if(!currentUser){
+        $.post('/api/users',{},(user)=>{
+            if(user){
+                console.log("logged in as ",user.username)
+                window.localStorage.user = JSON.stringify(user)
+                console.log("#nav-username")
+                $('#nav-username').text(user.username)
+            }
+        })
+    }else{
+        console.log('resuming session as ',currentUser.username)
+        console.log($("#nav-username"))
+        $('#nav-username').text(currentUser.username)
+    }
+
+    $("")
+
+}
+
+function loadPosts(){
+    $.get('/api/posts',(posts)=>{
+        for(let p of posts){
+            $('#posts-container').append(
+                $(
+                `<div class="card col-4 p-2 m-2 al" style="width: 18rem;">
+                <div class="card-body">
+                  <h5 class="card-title">${p.title}</h5>
+                  <h6 class="card-subtitle mb-2 text-muted">${p.user.username}</h6>
+                  <p class="card-text">${p.body.substr(0,200)}<a href='#'> ...read more</p>
+                  <a href="#" class="card-link">Comment</a>
+                  <a href="#" class="card-link">Like</a>
+                </div>
+              </div>`)
+            )
+        }
+    })
+
+    
+}
